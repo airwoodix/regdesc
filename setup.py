1e20a9b0da82d9364d7bdceb3c54d1eef4131dde
Etienne Wodey <wodey@iqo.uni-hannover.de>
2019-12-18 16:42:35 +0100

    setuptools

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="regdesc",
+    author="Etienne Wodey",
+    author_email="airwoodix@posteo.me",
+    description="Pythonic hardware register descriptors",
+    license="GPLv3",
+    use_scm_version=True,
+    packages=["regdesc", "regdesc.devices"],
+)
