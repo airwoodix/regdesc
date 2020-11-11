cda7d7a168fba5f2d883bea78db4453eb63ae4af
Etienne Wodey <wodey@iqo.uni-hannover.de>
2020-11-11 16:38:42 +0100

    Rename initial_value to reset_value. Expose each field's reset value.

--- regdesc/register.py
+++ regdesc/register.py
@@ -3,9 +3,10 @@ from . import utils
 
 
 class FieldDescriptor:
-    def __init__(self, shift, width):
+    def __init__(self, shift, width, reset_value):
         self.shift = int(shift)
         self.width = int(width)
+        self.reset_value = int(reset_value)
         self.mask = ((1 << self.width) - 1) << self.shift
 
     def __get__(self, reg, owner):
@@ -39,14 +40,14 @@ class RegisterMeta(type):
         # iterate through class attributes and transform
         # `Field` instances into corresponding descriptors
         shift = 0
-        dct["initial_value"] = 0
+        dct["reset_value"] = 0
         attrs = []
 
         for key, attr in dct.items():
             if isinstance(attr, Field):
                 attrs.append((key, attr))
-                dct[key] = FieldDescriptor(shift, attr.width)
-                dct["initial_value"] += attr.value << shift
+                dct[key] = FieldDescriptor(shift, attr.width, attr.value)
+                dct["reset_value"] += attr.value << shift
                 shift += attr.width
 
         dct["_fields"] = collections.OrderedDict(attrs)
@@ -56,15 +57,15 @@ class RegisterMeta(type):
 
 
 class Register(metaclass=RegisterMeta):
-    def __init__(self, *, initial_value=None, **kwds):
-        # override default initial value
-        if initial_value is not None:
-            initial_value = int(initial_value)
-            if initial_value.bit_length() >= self.width:
-                raise ValueError("initial_value too long")
-            self.initial_value = initial_value
+    def __init__(self, *, reset_value=None, **kwds):
+        # override default reset value
+        if reset_value is not None:
+            reset_value = int(reset_value)
+            if reset_value.bit_length() >= self.width:
+                raise ValueError("reset_value too long")
+            self.reset_value = reset_value
 
-        self.storage = self.initial_value
+        self.storage = self.reset_value
 
         # override default field values with passed keywords
         for name, value in kwds.items():
